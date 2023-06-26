be99dd1a07421ddaabfe0589d422851c187794e1
Flee-Time <koray509@gmail.com>
2023-06-26 23:00:15 +0300

    added sd card module cs pin init in stm32 hal

--- src/main.c
+++ src/main.c
@@ -191,7 +191,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA4 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_3;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
