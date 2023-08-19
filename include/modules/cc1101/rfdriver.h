0ffaf1b08f642d09d644790a6c3224acb2bf16a6
Flee-Time <koray509@gmail.com>
2023-08-19 11:33:40 +0300

    added cc1101 driver to rtos version

---
+++ include/modules/cc1101/rfdriver.h
@@ -0,0 +1,31 @@
+#ifndef __RFDRIVER_H
+#define __RFDRIVER_H
+
+#include "modules/cc1101/cc1101.h"
+#include "modules/cc1101/cc1101_configs.h"
+
+/** Radio Presets */
+typedef enum {
+    SubGhzPresetIDLE, /**< default configuration */
+    SubGhzPresetOok270Async, /**< OOK, bandwidth 270kHz, asynchronous */
+    SubGhzPresetOok650Async, /**< OOK, bandwidth 650kHz, asynchronous */
+    SubGhzPreset2FSKDev238Async, /**< FM, deviation 2.380371 kHz, asynchronous */
+    SubGhzPreset2FSKDev476Async, /**< FM, deviation 47.60742 kHz, asynchronous */
+    SubGhzPresetMSK99_97KbAsync, /**< MSK, deviation 47.60742 kHz, 99.97Kb/s, asynchronous */
+    SubGhzPresetGFSK9_99KbAsync, /**< GFSK, deviation 19.042969 kHz, 9.996Kb/s, asynchronous */
+    SubGhzPresetCustom, /**Custom Preset*/
+} SubGhzPreset;
+
+void rf_init();
+float rf_get_rssi();
+uint8_t rf_get_lqi();
+uint32_t rf_set_frequency(uint32_t value);
+bool rf_is_frequency_valid(uint32_t value);
+bool subghz_rx_pipe_not_empty();
+bool subghz_is_rx_data_crc_valid();
+void subghz_load_registers(uint8_t* data);
+void subghz_sleep();
+void subghz_load_preset(SubGhzPreset preset);
+void subghz_write_packet(const uint8_t* data, uint8_t size);
+
+#endif
